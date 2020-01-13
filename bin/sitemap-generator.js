702b5d7798fe32c759201422b189bdfebded04e5
xiangchao <vip_319@126.com>
2020-01-13 14:22:43 +0800

    添加备案信息

--- bin/sitemap-generator.js
+++ bin/sitemap-generator.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const SitemapGenerator = require('bin/sitemap-generator');
 
-const generator = SitemapGenerator('https://www.japanese-phrasebook.com/', {
+const generator = SitemapGenerator('https://www.nfwys.com/', {
   filepath: `${__dirname}/../public/sitemap.xml`,
   lastMod: true,
   stripQuerystring: true,
